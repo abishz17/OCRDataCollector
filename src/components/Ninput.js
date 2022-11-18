773ca8ec6c0d61a804406527049b4eb5ea054aa6
abishz17 <075bct008.abish@pcampus.edu.np>
2022-11-18 14:28:33 +0545

    Small typography div added

--- src/components/Ninput.js
+++ src/components/Ninput.js
@@ -1,24 +1,28 @@
 import React from "react";
 import TextField from "@mui/material/TextField";
-import { Button } from "@mui/material";
+import { Typography } from "@mui/material";
 
 const Ninput = (props) => {
   const dropdownChangeHandler = (event) => {
     props.getNumber(event.target.value);
   };
   return (
+    <div>
+      <Typography>Select the no. of lines</Typography>
+
       <TextField
         type="number"
         className="ninput"
+        defaultValue="5"
         InputProps={{
           inputProps: {
             max: 15,
             min: 1,
           },
         }}
-        label="No.of Lines"
         onChange={dropdownChangeHandler}
       />
+    </div>
   );
 };
 
