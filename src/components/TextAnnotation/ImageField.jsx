1f6608949be3b269612c851dc60f39c88acd2ff8
Gopal-Kshetri <075bct039.gopal@pcampus.edu.np>
2022-12-24 21:10:12 +0545

    const ip_addr added

--- src/components/TextAnnotation/ImageField.jsx
+++ src/components/TextAnnotation/ImageField.jsx
@@ -1,9 +1,10 @@
 import { React } from "react";
-const ImageUrl = "https://4.240.80.61:8000";
+import ip_addr from "../../utilities/constant.js";
+// const ImageUrl = "https://4.240.80.61:8000";
 
 const ImageField = (props) => {
   const handleClick = () => {
-    window.open(ImageUrl + props.image, "_blank");
+    window.open(ip_addr + props.image, "_blank");
   };
 
   return (
@@ -11,7 +12,7 @@ const ImageField = (props) => {
       <div className="relative m-auto mt-10 w-[80%] overflow-y-auto text-center h-[25%] bg-blue-100 rounded-md border-solid border-black border-2">
         <div className="">
           <img
-            src={ImageUrl + props.image}
+            src={ip_addr + props.image}
             alt={"Not available"}
             onClick={handleClick}
             className="lg:absolute flex justify-center items-center w-full h-full"
