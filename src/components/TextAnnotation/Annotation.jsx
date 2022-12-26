dd5cca4aed2422e3d47c5ae12e3e091488d53252
Gopal-Kshetri <075bct039.gopal@pcampus.edu.np>
2022-12-26 19:48:07 +0545

    romanized nepali added

--- src/components/TextAnnotation/Annotation.jsx
+++ src/components/TextAnnotation/Annotation.jsx
@@ -61,6 +61,7 @@ const Annotation = () => {
     });
   };
 
+
   useEffect(() => {
     getImage();
   }, []);
--- src/components/TextAnnotation/Textfield.jsx
+++ src/components/TextAnnotation/Textfield.jsx
@@ -1,36 +1,19 @@
 import { React, useState } from "react";
 import { TextField } from "@mui/material";
 import { styled } from "@mui/material/styles";
-
-const CssTextField = styled(TextField)({
-  "& label.Mui-focused": {
-    color: "none",
-  },
-  "& .MuiInput-underline:after": {
-    borderBottomColor: "none",
-  },
-  "& .MuiOutlinedInput-root": {
-    "& fieldset": {
-      borderColor: "none",
-    },
-    "&:hover fieldset": {
-      borderColor: "gray",
-    },
-    "&.Mui-focused fieldset": {
-      borderColor: "none",
-    },
-  },
-});
+import nepalify from "nepalify";
 
 const Textfield = (props) => {
   const handleChange = (event) => {
-    props.setText(event.target.value);
+    // props.setText(event.target.value);
+    props.setText(nepalify.format(event.target.value));
   };
 
   return (
-    <>
+    <>  
       <div className="bg-gray-100 ">
         <TextField
+          // id="nepali"
           variant="standard"
           label="Enter Text"
           helperText="Please enter text in Nepali"
