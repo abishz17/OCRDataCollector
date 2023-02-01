b0a24dfbaf1bf0792182d7defadf4fb282003a61
Manjeet Pandey <075bct048.manjeet@pacmpus.edu.np>
2023-02-01 11:51:57 +0545

    managed

--- OCRDataCollector/src/components/Feedback/FeedBackPopUp.jsx
+++
@@ -1,8 +0,0 @@
-import React, { useState } from "react";
-
-const FeedBackPopUp = () => {
-  const [open, setIsOpen] = useState(false);
-  return <div>{open && <p>Hello</p>}</div>;
-};
-
-export default FeedBackPopUp;
